7dc81974ecb2e7bbb69b3f27e75f7af709df7959
truespring <truespring1@gmail.com>
2021-10-06 20:07:18 +0900

    [feat] javascript this

---
+++ js-this.js
@@ -0,0 +1,19 @@
+function User(name) {
+	console.log('I\'m in "' + this.constructor.name + '" context.'); // "User"
+	this.name = name;
+	this.speak = function () {
+		console.log(this.name + ' is speaking from "' + this.constructor.name + '" context.'); // Ted, "User"
+		var drink = function () {
+			console.log('Drinking in "' + this.constructor.name + '"'); // "Window"
+		}
+		drink();
+	};
+	function ask() {
+		console.log('Asking from "' + this.constructor.name + '" context.'); // "Window"
+		console.log('Who am I? "' + this.name + '"'); // "Unknown"
+	}
+	ask();
+}
+var name = 'Unknown';
+var user = new User('Ted');
+user.speak();
