9dc5a279e09d77c4e60f4a86dc663f9982fa238b
truespring <truespring1@gmail.com>
2021-10-08 14:21:54 +0900

    [feat] add js-args

---
+++ js-ES6/js-another-args.js
@@ -0,0 +1,15 @@
+
+// ES5에서 arguments 활용
+fucntion workout(exercise1) {
+	var todos = Array.prototype.slice.call(arguments, workout.length);
+	console.log('Start from ' + exercise1);
+	console.log(todos.length + ' more to do');
+}
+
+// ES6에서 나머지 매개변수 활용용
+function workout(exercise1, ...todos) {
+	console.log('Start from ' + exericse1);		// Start from Treadmill
+	console.log(todos.length + ' more to do');	// 2 more to do
+	console.log('Args length: ' + workout.length);	// Args length: 1
+}
+workout('Treadmill', 'Pushup', 'Spinning');
